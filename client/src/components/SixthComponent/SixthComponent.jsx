5497a29b30c8df8bb0eceb90897de4c92788e720
Adrian <ad.rivarola18@gmail.com>
2024-09-02 20:10:26 -0500

    icon

--- client/src/components/SixthComponent/SixthComponent.jsx
+++ client/src/components/SixthComponent/SixthComponent.jsx
@@ -5,15 +5,17 @@ const SixthComponent = () => {
     const { news } = useFetchNews();
   return (
     <div className="sixth-container">
-      {news && (
-        <>
-          <h2>{news.title}</h2>
-          <a href={news.source.url} target="_blank">
-            {news.source.url}
-          </a>
-          <img src={news.image} alt="" />
-        </>
-      )}
+     
+        {news && (
+          <>
+            <h2>{news.title}</h2>
+            <a href={news.source.url} target="_blank">
+              {news.source.url}
+            </a>
+            <img src={news.image} alt="" />
+          </>
+        )}
+      
     </div>
   );
 };
