7a0b5d5926d82b6dc2e80bafdb77510449d39d23
Adrian <ad.rivarola18@gmail.com>
2024-09-08 23:42:07 -0500

    admin-dashboard

--- client/src/components/FifthComponent/FifthComponent.jsx
+++ client/src/components/FifthComponent/FifthComponent.jsx
@@ -1,18 +1,23 @@
 import "./FifthComponent.css";
 import { TbTruckDelivery } from "react-icons/tb";
 import { IoChatbubbleEllipses } from "react-icons/io5";
+import { MdDashboard } from "react-icons/md";
 
 const FifthComponent = () => {
   return (
     <div className="fifth-container">
       <h2 className="fifth-container-title">Projects</h2>
       <div className="projects-container">
-        <div >
+        <div>
           <TbTruckDelivery onClick={()=>window.open('https://food-delivery-frontend-vq0w.onrender.com')} />
           <p>Food Delivery</p>
         </div>
         <div>
           <IoChatbubbleEllipses onClick={()=>window.open('https://chat-app-2edbe.web.app')} />
+          <p>Chat App</p>
+        </div>
+        <div>
+          <MdDashboard onClick={()=>window.open('https://admin-dashboard-4txj.onrender.com/')} />
           <p>Chat App</p>
         </div>
       </div>
